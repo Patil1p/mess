3f091c55f4b1969a89e0084c4cce45a1d45a0aba
shri <shridharpatil3611@gmail.com>
2024-10-23 16:34:45 +0530

    mess

---
+++ src/app/shared/component/home-dashbord/home-dashbord.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-dashbord',
+  templateUrl: './home-dashbord.component.html',
+  styleUrl: './home-dashbord.component.scss'
+})
+export class HomeDashbordComponent {
+
+}
